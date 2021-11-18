a19fe63bdcf921a61dfcd6df243a0789c816b884
Diyorjon825 <diyorjonnasriddinov@gmail.com>
2021-11-18 09:55:18 +0500

    this is amazom app ui

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,356 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var displaysize = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: const Color(0xFF018197),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              height: displaysize.height / 100 * 14,
+              child: const Image(
+                image: AssetImage('assets/images/amazon_logo.png'),
+              ),
+            ),
+            SizedBox(
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.mic,
+                      size: displaysize.height / 100 * 3,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.shopping_cart,
+                      size: displaysize.height / 100 * 3,
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      body: ColoredBox(
+        color: Colors.grey.shade300,
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: displaysize.height / 100 * 7,
+              child: ColoredBox(
+                color: const Color(0xFF018197),
+                child: Container(
+                  margin:
+                      const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                  padding: const EdgeInsets.only(left: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.grey,
+                    ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Row(
+                    children: [
+                      const Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "What are you looking for?",
+                            hintStyle: TextStyle(color: Colors.grey),
+                            icon: Icon(
+                              Icons.search,
+                              color: Color(0xFF018197),
+                            ),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.camera_enhance,
+                          color: Color(0xFF018197),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    height: displaysize.height / 100 * 5,
+                    width: double.infinity,
+                    color: Colors.blueGrey,
+                    child: Row(
+                      children: const [
+                        Icon(Icons.location_on, color: Colors.white),
+                        SizedBox(width: 10),
+                        Text(
+                          "Deliver to Korea, Republic of",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                          height: displaysize.height / 100 * 15,
+                          child: ColoredBox(
+                            color: Colors.white,
+                            child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                image: const DecorationImage(
+                                  image:
+                                      AssetImage('assets/images/image_1.jpeg'),
+                                  fit: BoxFit.cover,
+                                ),
+                                borderRadius: BorderRadius.only(
+                                  bottomRight: Radius.circular(
+                                      displaysize.height / 100 * 7.5),
+                                  topRight: Radius.circular(
+                                      displaysize.height / 100 * 7.5),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(20),
+                          height: displaysize.height / 100 * 15,
+                          color: Colors.white,
+                          child: const Center(
+                            child: Text(
+                              "We ship 45million products",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Container(
+                    height: 155,
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Sign in for best experience',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 20)),
+                        Container(
+                          margin: const EdgeInsets.symmetric(vertical: 11),
+                          height: 45,
+                          color: Colors.orange,
+                          child: const Center(
+                            child: Text(
+                              "Sign in",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        ),
+                        const Text('Creat an account',
+                            style: TextStyle(color: Colors.blue, fontSize: 18)),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Deal of the Day",
+                          style: TextStyle(color: Colors.black, fontSize: 22),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Image(
+                          width: double.infinity,
+                          height: 240,
+                          image: AssetImage("assets/images/item_7.jpeg"),
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Text(
+                          "Up to 31% off APC UPS Battery Back",
+                          style: TextStyle(fontSize: 17),
+                        ),
+                        SizedBox(
+                          height: 6,
+                        ),
+                        Text(
+                          "\$10.99 - \$79.9",
+                          style: TextStyle(fontSize: 17),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Best sellers in Electronics",
+                          style: TextStyle(color: Colors.black, fontSize: 22),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.width,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  children: const [
+                                    Expanded(
+                                      child: Image(
+                                        image: AssetImage(
+                                            "assets/images/item_7.jpeg"),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Expanded(
+                                      child: Image(
+                                        image: AssetImage(
+                                            "assets/images/item_6.jpeg"),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(width: 5),
+                              Expanded(
+                                child: Column(
+                                  children: const [
+                                    Expanded(
+                                      child: Image(
+                                        image: AssetImage(
+                                            "assets/images/item_5.jpeg"),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Expanded(
+                                      child: Image(
+                                        image: AssetImage(
+                                            "assets/images/item_4.jpeg"),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Top products in Camera",
+                          style: TextStyle(color: Colors.black, fontSize: 22),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.width,
+                          child: Column(
+                            children: [
+                              const Expanded(
+                                child: Image(
+                                  width: double.infinity,
+                                  image:
+                                      AssetImage("assets/images/item_7.jpeg"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Expanded(
+                                child: Row(
+                                  children: const [
+                                    Expanded(
+                                      child: Image(
+                                        image: AssetImage(
+                                            "assets/images/item_6.jpeg"),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Expanded(
+                                      child: Image(
+                                        image: AssetImage(
+                                            "assets/images/item_5.jpeg"),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
